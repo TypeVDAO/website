05bb65d40dfcbe6fc889334d12e0c57192039846
0xRory <0x1rory@gmail.com>
2022-03-11 22:20:57 +0800

    flex
    flex

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './react-i18next-config';
+import ReactDOMServer from 'react-dom/server';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
